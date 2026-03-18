39f26c0e0160f8ec127315b83d1dde93113de55b
Toshiyuki Okamoto <nre46682@yahoo.co.jp>
2026-03-18 14:58:48 +0900

    update next config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  transpilePackages: [],
-  webpack: (config) => {
-    config.externals = [...(config.externals || []), 'playwright', 'bull', 'ioredis']
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals = [...(config.externals || []), 'playwright', 'bull', 'ioredis']
+    }
     return config
+  },
+  experimental: {
+    serverComponentsExternalPackages: ['playwright', 'bull', 'ioredis']
   }
 }
 module.exports = nextConfig
