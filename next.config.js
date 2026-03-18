ff64ab89de43f66a6dd9c49a3a771ce1b6745821
Toshiyuki Okamoto <nre46682@yahoo.co.jp>
2026-03-18 14:11:55 +0900

    fix next config webpack externals

--- next.config.js
+++ next.config.js
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: { serverComponentsExternalPackages: ['playwright', 'bull', 'ioredis'] }
+  transpilePackages: [],
+  webpack: (config) => {
+    config.externals = [...(config.externals || []), 'playwright', 'bull', 'ioredis']
+    return config
+  }
 }
 module.exports = nextConfig
